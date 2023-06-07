17dd3799b4fdc6add23e96fcb5e9387b34b66c1e
Rasmus Jonssønn Skramstad <rasmus.skramstad@gmail.com>
2023-06-07 09:49:33 +0200

    Første commit

---
+++ src/test/java/com/example/larsnotedatabase/LarsNotedatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.larsnotedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarsNotedatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
